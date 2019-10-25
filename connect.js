35671d07250add323517d76afbf40bbffaaf10c4
44tim44 <timeisrelative94@gmail.com>
2019-10-25 15:48:27 +0200

    Allt är najs

--- connect.js
+++ connect.js
@@ -1,9 +1,27 @@
+var currentQuestion = 0;
+
 socket.on('fromserver', message => {
     console.log(message);
 });
 
+socket.on('JoinResponse', message => {
+    console.log(message.response);
+});
+
+socket.on('AnswerResponse', message => {
+    console.log(message.response);
+});
+
+socket.on('QuizEnd', message => {
+    console.log(message);
+    currentQuestion = 0;
+    document.getElementById("currQ").innerHTML = "QuestionNumber: 0";
+});
+
 socket.on('NewQuestion', message => {
     console.log(message);
+    currentQuestion = message.QuestionNo;
+    document.getElementById("currQ").innerHTML = "QuestionNumber: " + message.QuestionNo.toString();
 });
 
 function join() {
@@ -26,5 +44,18 @@ function quit() {
     });
 }
 
+
+function banana() {
+    var pinCode = document.getElementById("pin-answer").value;
+    var username = document.getElementById("username-answer").value;
+    var answer = document.getElementById("answer-answer").value;
+
+    socket.emit('Answer', {
+        PIN: pinCode,
+        Username: username,
+        Answer: answer,
+        QuestionNo: currentQuestion
+    });
+}
 
 
