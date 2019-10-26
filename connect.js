cd3089b0ce151cb849cccd332b3ed92b9f4a24f5
44tim44 <timeisrelative94@gmail.com>
2019-10-26 15:00:16 +0200

    Fungerande Klient

--- connect.js
+++ connect.js
@@ -1,32 +1,70 @@
 var currentQuestion = 0;
+var pinCode;
+var username;
+var iframe = document.getElementById('bodyframe');
+var iframeDocument
+iframe.addEventListener("load", function() {
+    iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+});
 
 socket.on('fromserver', message => {
     console.log(message);
 });
 
 socket.on('JoinResponse', message => {
-    console.log(message.response);
+    if(message.flag == 0) {
+        console.log(message.response);
+        iframeDocument.getElementById("pin-input").value = "";
+    }
+    else if(message.flag == 1) {
+        console.log(message.response);
+        iframeDocument.getElementById("username-input").value = "";
+    }
+    else if(message.flag == 2) {
+        $('#bodyframe').attr('src','lobby.html');
+    }
 });
 
 socket.on('AnswerResponse', message => {
     console.log(message.response);
 });
 
-socket.on('QuizEnd', message => {
-    console.log(message);
-    currentQuestion = 0;
-    document.getElementById("currQ").innerHTML = "QuestionNumber: 0";
-});
 
 socket.on('NewQuestion', message => {
     console.log(message);
     currentQuestion = message.QuestionNo;
-    document.getElementById("currQ").innerHTML = "QuestionNumber: " + message.QuestionNo.toString();
+    const timeStart = message.TimeStart;
+    const timeStop = message.TimeStop;
+    const timeNow = Date.now();
+
+    setTimeout(function(){
+        $('#bodyframe').attr('src','question.html');
+        iframe.addEventListener("load", function() {
+            iframeDocument.getElementById("currQ").innerHTML = "Question " + (message.QuestionNo + 1).toString();
+            iframeDocument.getElementById("answer-button-1").disabled = false;
+            iframeDocument.getElementById("answer-button-2").disabled = false;
+            iframeDocument.getElementById("answer-button-3").disabled = false;
+            iframeDocument.getElementById("answer-button-4").disabled = false;
+        });
+        }, (timeStart-timeNow));
+
+    setTimeout(function(){
+        iframeDocument.getElementById("answer-button-1").disabled = true;
+        iframeDocument.getElementById("answer-button-2").disabled = true;
+        iframeDocument.getElementById("answer-button-3").disabled = true;
+        iframeDocument.getElementById("answer-button-4").disabled = true;
+    }, (timeStop-timeNow));
+});
+
+socket.on('QuizEnd', message => {
+    console.log(message);
+    currentQuestion = 0;
+    $('#bodyframe').attr('src','lobby.html');
 });
 
 function join() {
-    var pinCode = document.getElementById("pin-input").value;
-    var username = document.getElementById("username-input").value;
+    pinCode = iframeDocument.getElementById("pin-input").value;
+    username = iframeDocument.getElementById("username-input").value;
 
     socket.emit('Join', {
         PIN: pinCode,
@@ -35,9 +73,6 @@ function join() {
 }
 
 function quit() {
-    var pinCode = document.getElementById("pin-input").value;
-    var username = document.getElementById("username-input").value;
-
     socket.emit('Quit', {
         PIN: pinCode,
         Username: username
@@ -59,3 +94,17 @@ function banana() {
 }
 
 
+function answer(id) {
+
+    iframeDocument.getElementById("answer-button-1").disabled = true;
+    iframeDocument.getElementById("answer-button-2").disabled = true;
+    iframeDocument.getElementById("answer-button-3").disabled = true;
+    iframeDocument.getElementById("answer-button-4").disabled = true;
+
+    socket.emit('Answer', {
+        PIN: pinCode,
+        Username: username,
+        Answer: id,
+        QuestionNo: currentQuestion
+    });
+}
